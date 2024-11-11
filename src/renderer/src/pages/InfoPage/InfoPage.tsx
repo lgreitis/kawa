453b7fd62250b03dfea7cde58696012ffb4c78b1
lgreitis <lukas.greicius@ktu.edu>
2024-11-11 05:58:58 +0200

    fix: dont show related anime header if empty

--- src/renderer/src/pages/InfoPage/InfoPage.tsx
+++ src/renderer/src/pages/InfoPage/InfoPage.tsx
@@ -120,31 +120,37 @@ export const InfoPage: React.FC = () => {
               {malData.studios.map((studio) => studio.name).join(", ")}
             </p>
 
-            <h3 className="mb-4 mt-3 text-2xl font-semibold">Related anime</h3>
-            <div className="grid grid-cols-3 gap-4 overflow-hidden sm:grid-cols-4 2xl:grid-cols-6">
-              {malData.related_anime.slice(0, 7).map((related) => (
-                <button
-                  key={related.node.id}
-                  className="relative flex flex-col text-left"
-                  onClick={() => navigate(`/info/${related.node.id}`)}
-                >
-                  <img
-                    className="aspect-[2/3] rounded-2xl object-cover"
-                    src={related.node.main_picture.medium}
-                  />
-                  <span className="line-clamp-2">{related.node.title}</span>
-                  <span className="text-sm text-zinc-300">{related.relation_type_formatted}</span>
-                </button>
-              ))}
-              {malData.related_anime.length > 7 && (
-                <button
-                  className="flex aspect-[2/3] items-center justify-center rounded-2xl bg-black/40"
-                  onClick={() => toast.error("Not implemented")}
-                >
-                  <span>View more</span>
-                </button>
-              )}
-            </div>
+            {!!malData.related_anime.length && (
+              <React.Fragment>
+                <h3 className="mb-4 mt-3 text-2xl font-semibold">Related anime</h3>
+                <div className="grid grid-cols-3 gap-4 overflow-hidden sm:grid-cols-4 2xl:grid-cols-6">
+                  {malData.related_anime.slice(0, 7).map((related) => (
+                    <button
+                      key={related.node.id}
+                      className="relative flex flex-col text-left"
+                      onClick={() => navigate(`/info/${related.node.id}`)}
+                    >
+                      <img
+                        className="aspect-[2/3] rounded-2xl object-cover"
+                        src={related.node.main_picture.medium}
+                      />
+                      <span className="line-clamp-2">{related.node.title}</span>
+                      <span className="text-sm text-zinc-300">
+                        {related.relation_type_formatted}
+                      </span>
+                    </button>
+                  ))}
+                  {malData.related_anime.length > 7 && (
+                    <button
+                      className="flex aspect-[2/3] items-center justify-center rounded-2xl bg-black/40"
+                      onClick={() => toast.error("Not implemented")}
+                    >
+                      <span>View more</span>
+                    </button>
+                  )}
+                </div>
+              </React.Fragment>
+            )}
           </div>
 
           <div className="flex w-full flex-col lg:col-span-2 lg:max-w-2xl">
